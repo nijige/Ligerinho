7ef95506335014b2542879f83d8d108da5297a69
Geane Ferreira Pereira <geane3096@gmail.com>
2021-05-24 18:37:41 -0400

    adicionando o calculo e reset á aplicação

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,11 +5,20 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   var _gasCtrl = new MoneyMaskedTextController();
+
   var _alcCtrl = new MoneyMaskedTextController();
+
   var _busy = false;
+
   var _completed = true;
+
   var _resultText = "Compensa utilizar álcool";
 
   @override
@@ -20,13 +29,56 @@ class HomePage extends StatelessWidget {
           children: <Widget>[
             Logo(),
             _completed
-                ? SucessWidget(result: _resultText, reset: () {})
+                ? SucessWidget(
+                  result: _resultText,
+                   reset: reset,
+                )
                 : SubmitForm(
                     alcCtrl: _alcCtrl,
                     gasCtrl: _gasCtrl,
-                    busy: false,
-                    submitFun: () {})
+                    busy: _busy,
+                    submitFun: calculate,
+                  ),
           ],
         ));
+  }
+
+  Future calculate() {
+    double alc =
+        double.parse(_alcCtrl.text.replaceAll(new RegExp(r'[,.]'), '')) / 100;
+
+    double gas =
+        double.parse(_gasCtrl.text.replaceAll(new RegExp(r'[,.]'), '')) / 100;
+
+    double rest = alc / gas;
+
+    setState(() {
+      _completed = false;
+      _busy = true;
+    });
+
+    return new Future.delayed(
+        const Duration(seconds: 2),
+        () => {
+              setState(() {
+                if (rest >= 0.7) {
+                  _resultText = "Compensa utilizar Gasolina";
+                } else {
+                  _resultText = "Compensa utilizar Álcool";
+                }
+
+                _completed = false;
+                _busy = true;
+              })
+            });
+  }
+
+  reset() {
+    setState(() {
+      _alcCtrl = new MoneyMaskedTextController();
+      _gasCtrl = new MoneyMaskedTextController();
+      _completed = false;
+      _busy = false;
+    });
   }
 }
